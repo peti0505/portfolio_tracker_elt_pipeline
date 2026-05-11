587dcff2bbd49530ba0d6104623c3c09b3818426
phauzer <phauzer05@gmail.com>
2026-05-11 23:19:43 +0200

    feat: add error handling and logging to the Extract-Load part

--- extract_load/E_L.py
+++ extract_load/E_L.py
@@ -5,240 +5,406 @@ import pandas as pd
 import gspread
 import pandas_gbq
 from google.oauth2 import service_account
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatting = logging.Formatter(
+    "%(asctime)s-%(levelname)s-%(name)s-%(funcName)s-%(message)s"
+)
+file = logging.FileHandler("dataloading.log")
+file.setFormatter(formatting)
+logger.addHandler(file)
 
 
 def get_conf() -> dict:
 
-    load_dotenv()
+    try:
+        load_dotenv()
 
-    conf = {
-        "tiingo_api_token": os.environ.get("tiingo_api_token"),
-        "json_file_path": os.environ.get("gc_service_account_json_path"),
-        "credentials": service_account.Credentials.from_service_account_file(
-            os.environ.get("gc_service_account_json_path")
-        ),
-        "project": os.environ.get("project_id"),
-        "dataset": os.environ.get("dataset_name"),
-        "transactions_table": os.environ.get("dataset_name") + "." + "raw_transactions",
-    }
+        conf = {
+            "tiingo_api_token": os.environ.get("tiingo_api_token"),
+            "json_file_path": os.environ.get("gc_service_account_json_path"),
+            "credentials": service_account.Credentials.from_service_account_file(
+                os.environ.get("gc_service_account_json_path")
+            ),
+            "project": os.environ.get("project_id"),
+            "dataset": os.environ.get("dataset_name"),
+            "transactions_table": os.environ.get("dataset_name")
+            + "."
+            + "raw_transactions",
+        }
 
-    return conf
+    except:
+        logger.exception("Couldn't read .env file.")
+
+        raise
+
+    else:
+        logger.info("Config read successfully from .env file.")
+
+        return conf
 
 
-def googlesheet_to_gbq(conf: dict) -> None:
+def get_googlesheet_transactions(conf: dict) -> pd.DataFrame:
 
-    gc = gspread.service_account(filename=conf["json_file_path"])
+    try:
+        gc = gspread.service_account(filename=conf["json_file_path"])
+        sh = gc.open("portfolio_transactions")
+        worksheet = sh.worksheet("Transactions")
 
-    sh = gc.open("portfolio_transactions")
-    worksheet = sh.worksheet("Transactions")
-    transaction_dict = worksheet.get_all_records()
+        transaction_dict = worksheet.get_all_records()
+        df_transactions = pd.DataFrame(transaction_dict)
 
-    df_transactions = pd.DataFrame(transaction_dict)
+    except:
+        logger.exception("Couldn't read transactions from google sheets.")
 
-    pandas_gbq.to_gbq(
-        df_transactions,
-        conf["transactions_table"],
-        project_id=conf["project"],
-        if_exists="append",
-        credentials=conf["credentials"],
-    )
+        df_transactions = pd.DataFrame()
 
-    worksheet.batch_clear(["A2:E1000"])
+        return df_transactions
+
+    else:
+
+        if df_transactions.empty:
+            logger.info("The transactions sheet was empty.")
+
+        else:
+            logger.info("Transactions read successfully.")
+
+        return df_transactions
+
+
+def transactions_to_gbq(transactions: pd.DataFrame, conf: dict) -> bool:
+
+    if transactions.empty:
+        logger.info("Couldn't load transactions, transactions were empty.")
+
+        return False
+
+    else:
+        try:
+            pandas_gbq.to_gbq(
+                transactions,
+                conf["transactions_table"],
+                project_id=conf["project"],
+                if_exists="append",
+                credentials=conf["credentials"],
+            )
+
+        except:
+            logger.exception("Couldn't load transactions into Google BigQuery.")
+
+            return False
+
+        else:
+            logger.info("Transactions loaded into Google BigQuery successfully.")
+
+            return True
+
+
+def clean_google_sheets(switch: bool, conf: dict) -> None:
+
+    if switch == True:
+        try:
+            gc = gspread.service_account(filename=conf["json_file_path"])
+            sh = gc.open("portfolio_transactions")
+            worksheet = sh.worksheet("Transactions")
+
+            worksheet.batch_clear(["A2:E1000"])
+
+        except:
+            logger.exception("Couldn't clear Google Sheets transactions")
+
+        else:
+            logger.info("Google Sheets cleaned successfully.")
+
+    else:
+        logger.info(
+            "Couldn't clear the sheet, no transactions were loaded to Google BigQuery."
+        )
 
 
 def gbq_get_tickers(conf: dict) -> list:
 
-    sql = f"""
-    WITH ticker_total AS
-    (
-    SELECT
-    Ticker,
-    sum(`Amount + for buy - for sell`) as Current_total
-    FROM {conf["transactions_table"]}
-    GROUP BY 1
-    HAVING sum(`Amount + for buy - for sell`) > 0
-    )
-    SELECT
-    Ticker
-    FROM
-    ticker_total
-    """
+    try:
+        sql = f"""
+        WITH ticker_total AS
+        (
+        SELECT
+        Ticker,
+        sum(`Amount + for buy - for sell`) as Current_total
+        FROM {conf["transactions_table"]}
+        GROUP BY 1
+        HAVING sum(`Amount + for buy - for sell`) > 0
+        )
+        SELECT
+        Ticker
+        FROM
+        ticker_total
+        """
 
-    df_tickers = pandas_gbq.read_gbq(
-        sql, project_id=conf["project"], credentials=conf["credentials"]
-    )
+        df_tickers = pandas_gbq.read_gbq(
+            sql, project_id=conf["project"], credentials=conf["credentials"]
+        )
 
-    tickers = df_tickers["Ticker"].to_list()
+        tickers = df_tickers["Ticker"].to_list()
 
-    return tickers
+    except:
+        logger.exception("Couldn't fetch the actove tickers from Google BigQuery.")
+
+        raise
+
+    else:
+        logger.info("Currently active tickers fetched succesfully.")
+
+        return tickers
 
 
 def gbq_get_all_tickers(conf: dict) -> list:
 
-    sql = f"""
-    SELECT
-    Ticker
-    FROM {conf["transactions_table"]}
-    GROUP BY 1
-    """
+    try:
+        sql = f"""
+        SELECT
+        Ticker
+        FROM {conf["transactions_table"]}
+        GROUP BY 1
+        """
 
-    df_tickers = pandas_gbq.read_gbq(
-        sql, project_id=conf["project"], credentials=conf["credentials"]
-    )
+        df_tickers = pandas_gbq.read_gbq(
+            sql, project_id=conf["project"], credentials=conf["credentials"]
+        )
 
-    tickers = df_tickers["Ticker"].to_list()
+        tickers = df_tickers["Ticker"].to_list()
 
-    return tickers
+    except:
+        logger.exception("Couldn't fetch all time tickers from Google BigQuery.")
+
+        raise
+
+    else:
+        logger.info("All time tickers fetched successfully")
+
+        return tickers
 
 
 def gbq_get_currencies(conf: dict) -> list:
 
-    sql = f"""
-    WITH ticker_total AS
-    (
-    SELECT
-    Ticker,
-    `Currency code`,
-    sum(`Amount + for buy - for sell`) as Current_total
-    FROM {conf["transactions_table"]}
-    GROUP BY 1, 2
-    HAVING sum(`Amount + for buy - for sell`) > 0
-    )
-    SELECT DISTINCT
-    `Currency code`
-    FROM
-    ticker_total
-    WHERE `Currency code` != 'EUR'
-    """
+    try:
+        sql = f"""
+        WITH ticker_total AS
+        (
+        SELECT
+        Ticker,
+        `Currency code`,
+        sum(`Amount + for buy - for sell`) as Current_total
+        FROM {conf["transactions_table"]}
+        GROUP BY 1, 2
+        HAVING sum(`Amount + for buy - for sell`) > 0
+        )
+        SELECT DISTINCT
+        `Currency code`
+        FROM
+        ticker_total
+        WHERE `Currency code` != 'EUR'
+        """
 
-    df_currencies = pandas_gbq.read_gbq(
-        sql, project_id=conf["project"], credentials=conf["credentials"]
-    )
+        df_currencies = pandas_gbq.read_gbq(
+            sql, project_id=conf["project"], credentials=conf["credentials"]
+        )
 
-    currencies = df_currencies["Currency code"].to_list()
+        currencies = df_currencies["Currency code"].to_list()
 
-    return currencies
+    except:
+        logger.exception("Couldn't fetch unique currencies from Google BigQuery.")
+
+        raise
+
+    else:
+        logger.info("Unique currencies fetched successfully.")
+
+        return currencies
 
 
 def api_get_asset_metadata(asset_tickers: list, conf: dict) -> pd.DataFrame:
 
-    metadata_list = []
-    for ticker in asset_tickers:
+    try:
+        metadata_list = []
+        for ticker in asset_tickers:
 
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": conf["tiingo_api_token"],
-        }
+            headers = {
+                "Content-Type": "application/json",
+                "Authorization": conf["tiingo_api_token"],
+            }
 
-        response = requests.get(
-            f"https://api.tiingo.com/tiingo/daily/{ticker}", headers=headers
-        )
+            response = requests.get(
+                f"https://api.tiingo.com/tiingo/daily/{ticker}", headers=headers
+            )
 
-        r = response.json()
-        metadata = {
-            "tickers": ticker,
-            "asset_name": r["name"],
-            "exchange_code": r["exchangeCode"],
-        }
-        metadata_list.append(metadata)
+            r = response.json()
+            metadata = {
+                "tickers": ticker,
+                "asset_name": r["name"],
+                "exchange_code": r["exchangeCode"],
+            }
+            metadata_list.append(metadata)
 
-    df_metadata = pd.DataFrame(metadata_list)
-    return df_metadata
+        df_metadata = pd.DataFrame(metadata_list)
+
+    except:
+        logger.exception("Couldn't fetch API asset metadata infos.")
+
+        raise
+
+    else:
+        logger.info("API asset metadata infos fetched successfully.")
+
+        return df_metadata
 
 
 def api_get_asset_price(asset_tickers: list, conf: dict) -> pd.DataFrame:
 
-    price_list = []
-    for ticker in asset_tickers:
+    try:
+        price_list = []
+        for ticker in asset_tickers:
 
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": conf["tiingo_api_token"],
-        }
+            headers = {
+                "Content-Type": "application/json",
+                "Authorization": conf["tiingo_api_token"],
+            }
 
-        response = requests.get(
-            f"https://api.tiingo.com/tiingo/daily/{ticker}/prices", headers=headers
-        )
+            response = requests.get(
+                f"https://api.tiingo.com/tiingo/daily/{ticker}/prices", headers=headers
+            )
 
-        r = response.json()
-        price = {"dates": r[0]["date"], "tickers": ticker, "price": r[0]["adjClose"]}
-        price_list.append(price)
+            r = response.json()
+            price = {
+                "dates": r[0]["date"],
+                "tickers": ticker,
+                "price": r[0]["adjClose"],
+            }
+            price_list.append(price)
 
-    df_prices = pd.DataFrame(price_list)
+        df_prices = pd.DataFrame(price_list)
 
-    return df_prices
+    except:
+        logger.exception("Couldn't fetch API asset prices.")
+
+        raise
+
+    else:
+        logger.info("API asset prices fetched successfully.")
+
+        return df_prices
 
 
 def api_get_currency_rate(currency_list: list) -> pd.DataFrame:
 
-    currency_list = ",".join(currency_list)
+    try:
+        currency_list = ",".join(currency_list)
 
-    response = requests.get(
-        f"https://api.frankfurter.dev/v2/rates?quotes={currency_list}"
-    )
-    r = response.json()
+        response = requests.get(
+            f"https://api.frankfurter.dev/v2/rates?quotes={currency_list}"
+        )
+        r = response.json()
 
-    currencies = []
-    for i in r:
-        currencies_row = {
-            "dates": i["date"],
-            "currency_code": i["quote"],
-            "rate": i["rate"],
-        }
-        currencies.append(currencies_row)
+        currencies = []
+        for i in r:
+            currencies_row = {
+                "dates": i["date"],
+                "currency_code": i["quote"],
+                "rate": i["rate"],
+            }
+            currencies.append(currencies_row)
 
-    df_currencies = pd.DataFrame(currencies)
+        df_currencies = pd.DataFrame(currencies)
 
-    return df_currencies
+    except:
+        logger.exception("Couldn't fetch API currency rates.")
+
+        raise
+
+    else:
+        logger.info("API currency rates fetched successfully.")
+
+        return df_currencies
 
 
-def asset_names_to_gbq(names: pd.DataFrame, conf: dict) -> None:
+def asset_metadatas_to_gbq(names: pd.DataFrame, conf: dict) -> None:
 
-    table = conf["dataset"] + "." + "raw_asset_metadatas"
-    pandas_gbq.to_gbq(
-        names,
-        table,
-        project_id=conf["project"],
-        if_exists="replace",
-        credentials=conf["credentials"],
-    )
+    try:
+        table = conf["dataset"] + "." + "raw_asset_metadatas"
+        pandas_gbq.to_gbq(
+            names,
+            table,
+            project_id=conf["project"],
+            if_exists="replace",
+            credentials=conf["credentials"],
+        )
+
+    except:
+        logger.exception("Couldn't load asset metadata infos into Google BigQuery.")
+
+        raise
+
+    else:
+        logger.info("Asset metadata infos loaded into Google BigQuery successfully.")
 
 
 def asset_prices_to_gbq(prices: pd.DataFrame, conf: dict) -> None:
 
-    table = conf["dataset"] + "." + "raw_asset_prices"
-    pandas_gbq.to_gbq(
-        prices,
-        table,
-        project_id=conf["project"],
-        if_exists="append",
-        credentials=conf["credentials"],
-    )
+    try:
+        table = conf["dataset"] + "." + "raw_asset_prices"
+        pandas_gbq.to_gbq(
+            prices,
+            table,
+            project_id=conf["project"],
+            if_exists="append",
+            credentials=conf["credentials"],
+        )
+
+    except:
+        logger.exception("Couldn't load asset prices into Google BigQuery.")
+
+        raise
+
+    else:
+        logger.info("Asset prices loaded into Google BigQuery successfully.")
 
 
 def currency_rates_to_gbq(currencies: pd.DataFrame, conf: dict) -> None:
 
-    table = conf["dataset"] + "." + "raw_currency_exchange_price"
-    pandas_gbq.to_gbq(
-        currencies,
-        table,
-        project_id=conf["project"],
-        if_exists="append",
-        credentials=conf["credentials"],
-    )
+    try:
+        table = conf["dataset"] + "." + "raw_currency_exchange_price"
+        pandas_gbq.to_gbq(
+            currencies,
+            table,
+            project_id=conf["project"],
+            if_exists="append",
+            credentials=conf["credentials"],
+        )
+
+    except:
+        logger.exception("Couldn't load currency rates into Google BigQuery.")
+
+        raise
+
+    else:
+        logger.info("Currency rates loaded into Google BigQuery successfully.")
 
 
 def main() -> None:
 
     conf = get_conf()
-    googlesheet_to_gbq(conf)
+    df_transactions = get_googlesheet_transactions(conf)
+    switch = transactions_to_gbq(df_transactions, conf)
+    clean_google_sheets(switch, conf)
     tickers = gbq_get_tickers(conf)
     all_tickers = gbq_get_all_tickers(conf)
     currencies = gbq_get_currencies(conf)
     asset_metadatas = api_get_asset_metadata(all_tickers, conf)
     asset_prices = api_get_asset_price(tickers, conf)
     currency_rates = api_get_currency_rate(currencies)
-    asset_names_to_gbq(asset_metadatas, conf)
+    asset_metadatas_to_gbq(asset_metadatas, conf)
     asset_prices_to_gbq(asset_prices, conf)
     currency_rates_to_gbq(currency_rates, conf)
 
