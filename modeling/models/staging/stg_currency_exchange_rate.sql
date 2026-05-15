943a9c3ef933a1a760a4150e46c7b3ea819b4950
phauzer <phauzer05@gmail.com>
2026-05-15 15:15:57 +0200

    fix: deduplicate dates when there are multiple rates for a day

--- modeling/models/staging/stg_currency_exchange_rate.sql
+++ modeling/models/staging/stg_currency_exchange_rate.sql
@@ -1,11 +1,14 @@
 WITH staging AS 
 (
-SELECT DISTINCT
+SELECT
     CAST(dates AS DATE) AS date, 
     UPPER(currency_code) AS currency_code,
-    rate
+    --rate is not closing rate, so i avg the intraday rates
+    AVG(rate) as rate
 FROM 
     {{source('portfolio', 'raw_currency_exchange_price')}}
+GROUP BY
+    1,2
 )
 
 SELECT * FROM staging
